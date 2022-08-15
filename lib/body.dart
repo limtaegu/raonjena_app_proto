08719be4fa54bc74eaa876c077f2c796fc7f2494
limtaegu <aka1997@naver.com>
2022-08-15 16:02:24 +0900

    raonjena_app_prototype 0.5 TextStyle 적용

--- lib/body.dart
+++ lib/body.dart
@@ -6,21 +6,39 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink,
       appBar: AppBar(
         title: Text('라온제나 앱'),
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Color.fromARGB(255, 255, 182, 206),
         elevation: 0.0,
       ),
       // AppBar
 
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start, // start 시작점 정렬
           children: <Widget>[
-            Text('홈'),
-            Text('예약'),
-            Text('문의'),
-            Text('마이페이지'),
+            Text(
+              "홈",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "예약",
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text("문의"),
+            Text("마이페이지"),
           ],
         ),
       ),
