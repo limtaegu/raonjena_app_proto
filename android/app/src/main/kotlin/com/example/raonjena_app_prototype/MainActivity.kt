1c2a3d34373c7ee4cef8d242ab4b46a0ea1ae1d5
limtaegu <aka1997@naver.com>
2022-08-15 13:19:01 +0900

    raonjena_app_prototype 0.1

---
+++ android/app/src/main/kotlin/com/example/raonjena_app_prototype/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.raonjena_app_prototype
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
+
+class name extends StatefulWidget {
+  name({Key? key}) : super(key: key);
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
